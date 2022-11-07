a0c94941c81497837e585727bd6fc85052ca145c
Akshay Shankar <ashankar@pi5.physik.uni-stuttgart.de>
2022-11-07 11:51:06 +0100

    plot convergence of CMFT

--- src/Solving_BHM/1D/CMFT/CMFTScript.jl
+++ src/Solving_BHM/1D/CMFT/CMFTScript.jl
@@ -1,3 +1,5 @@
+using JLD
+
 begin 
 	ln = 100
 	t = range(start = 0, stop = 0.15, length = ln)
@@ -5,15 +7,15 @@ begin
 	
 	order_param = zeros((ln, ln))
 
-    num_sites = 4
+    num_sites = 6
     nbosons = 4
     model = BHM(num_sites, nbosons, false, 0);
 
-    Threads.@threads for k1 in 1:ln
+    @time Threads.@threads for k1 in 1:ln
         for k2 in 1:ln
             order_param[k2, k1] = abs(get_order_parameter(model, t[k1], mu[k2]))
         end 
     end
 
-    save("../data/cmft/L=$(num_sites)_nb=$(nbosons)_n=$(ln).jld", "t", t, "mu", mu, "order_param", order_param, "L", num_sites, "nbosons", nbosons)
+    save("../data/cmft/1D_L=$(num_sites)_nb=$(nbosons)_n=$(ln).jld", "t", t, "mu", mu, "order_param", order_param, "L", num_sites, "nbosons", nbosons)
 end
